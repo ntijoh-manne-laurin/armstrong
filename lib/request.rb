465d748aaf6e7dc17d84b08c47cc46d9b90cb76b
ManneMyten <manne.laurin@elev.ga.ntig.se>
2024-11-04 10:15:08 +0100

    fler tester klara

--- lib/request.rb
+++ lib/request.rb
@@ -1,18 +1,25 @@
 require_relative 'utils'
 
 class Request
-  
-  attr_reader :method, :resource, :version, :headers
+	
+	attr_reader :method, :resource, :version, :headers, :params
 
-  def initialize(request_string)
-    rows = request_string.split("\n")
-    first_row, *rest = rows
-    @method, @resource, @version = first_row.split(" ")
-    # keys = header.map {|n| n.split(': ')[0]}
-    # values = header.map {|n| n.split(': ')[1]}
+	def find_params
+		if @method == 'GET'
+			_, param_string = @resource.split('?')
+			if param_string == nil
+				return nil
+			else
+				param_string.split('&').map{|set| set.split('=')}.to_h
+			end
+		end
+	end
 
-    @headers = Utils.arr_to_hash(rest)
-    
-    # @headers = {|k,v| k.split}
-  end
+	def initialize(request_string)
+		rows = request_string.split("\n")
+		first_row, *rest = rows
+		@method, @resource, @version = first_row.split(" ")
+		@headers = rest.map{|e| e.split(": ", 2)}.to_h
+		@params = find_params
+	end
 end
--- test/http_parser_spec.rb
+++ test/http_parser_spec.rb
@@ -30,6 +30,67 @@ describe 'Request' do
             _(request.headers).must_equal headers
         end
     end
+	describe 'Get-request with more headers' do
 
+		it 'parses the http method' do
+            request_string = File.read('./test/example_requests/get-examples.request.txt')
+            request = Request.new(request_string)
+            _(request.method).must_equal "GET"
+        end
+
+		it 'parses the resource' do
+            request_string = File.read('./test/example_requests/get-examples.request.txt')
+            request = Request.new(request_string)
+            _(request.resource).must_equal "/examples"
+        end
+
+		it 'parses the version' do
+            request_string = File.read('./test/example_requests/get-examples.request.txt')
+            request = Request.new(request_string)
+            _(request.version).must_equal "HTTP/1.1"
+        end
+
+		it 'parses the headers' do
+            request_string = File.read('./test/example_requests/get-examples.request.txt')
+            request = Request.new(request_string)
+            headers =  {'Host' => 'example.com', 'User-Agent' => 'ExampleBrowser/1.0', 'Accept-Encoding' => 'gzip, deflate', 'Accept' => '*/*'}
+            _(request.headers).must_equal headers
+        end
+	end
+
+	describe 'Get-request with parameters' do 
+		
+		it 'parses the http method' do
+            request_string = File.read('./test/example_requests/get-fruits-with-filter.request.txt')
+            request = Request.new(request_string)
+            _(request.method).must_equal "GET"
+        end
+
+		it 'parses the resource' do
+            request_string = File.read('./test/example_requests/get-fruits-with-filter.request.txt')
+            request = Request.new(request_string)
+            _(request.resource).must_equal "/fruits?type=bananas&minrating=4"
+        end
+
+		it 'parses the version' do
+            request_string = File.read('./test/example_requests/get-fruits-with-filter.request.txt')
+            request = Request.new(request_string)
+            _(request.version).must_equal "HTTP/1.1"
+        end
+
+		it 'parses the headers' do
+            request_string = File.read('./test/example_requests/get-fruits-with-filter.request.txt')
+            request = Request.new(request_string)
+            headers =  {'Host' => 'fruits.com', 'User-Agent' => 'ExampleBrowser/1.0', 'Accept-Encoding' => 'gzip, deflate', 'Accept' => '*/*'}
+            _(request.headers).must_equal headers
+        end
+
+		it 'parses the parameters' do
+			request_string = File.read('./test/example_requests/get-fruits-with-filter.request.txt')
+            request = Request.new(request_string)
+			params =  {'type' => 'bananas', 'minrating' => '4'}
+			_(request.params).must_equal params
+		end
+	end
 
 end
