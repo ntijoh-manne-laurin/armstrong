59b3d0b3b42a5ed3e4c73becbc5ad2ac0dfabb1a
ManneMyten <manne.laurin@elev.ga.ntig.se>
2025-01-13 09:59:42 +0100

    börjat med variabler i routes

--- lib/router.rb
+++ lib/router.rb
@@ -1,3 +1,5 @@
+require 'debug'
+
 class Router
   def initialize
     @routes = [] # [{method: "get", resource: "/banan", block: "..."}, {}]
@@ -13,18 +15,47 @@ class Router
   def match_route(request)
     @routes.each do |route|
       if route[:resource].include?(":")
+        params = []
         resource = route[:resource].split("/")
-        resource.each {|p| p.include?(":") params << p}
+        resource.each do |param| 
+          if param.include?(":") 
+            binding.break
+            params << param
+          end
+        end
+
+        resource_string = ""
+        route[:resource].each do |element|
+          if element == ":"
+            break
+          end
+          resource_string << element
+        end
+
+        i = 0
+        while route[:resource][i] != ":"
+          if route[:resource][i] == request[i]
+        end
+
+        if route[:method] == request.method
+          route = route
+          break
+        end
+
+      else
+        if route[:method] == request.method && route[:resource] == request.resource
+          route = route
+          break
+        end
       end
     end
 
-    route = @routes.find {|route| (route[:method] == request.method && route[:resource] == request.resource)}
     if route == nil
       return nil
     else
       require 'debug'
       binding.break
-      route[:block].call
+      route[:block].call(params)
     end
   end
 end
